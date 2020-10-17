2a94c53c76806070ec0ebcd55f6be10199aaa2e5
ahmed-debbech <debbech.ahmed@gmail.com>
2020-10-18 00:56:08 +0100

    add pass to main activity through login

--- app/src/main/java/com/ahmeddebbech/aries_messenger/LoginActivity.java
+++ app/src/main/java/com/ahmeddebbech/aries_messenger/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.ahmeddebbech.aries_messenger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -30,6 +33,21 @@ public class LoginActivity extends AppCompatActivity {
                 public void onClick(View view){
                     dl.openDrawer(GravityCompat.START);
                 }
+        });
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_settings1:
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+                //close navigation drawer
+                dl.closeDrawer(GravityCompat.START);
+                return true;
+            }
         });
     }
 }
