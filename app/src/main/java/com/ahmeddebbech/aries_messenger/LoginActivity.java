5a79765413e779e6b0a2862fcf7baa1a9e280202
ahmed-debbech <debbech.ahmed@gmail.com>
2020-10-24 22:48:54 +0100

    add database class and package

--- app/src/main/java/com/ahmeddebbech/aries_messenger/LoginActivity.java
+++ app/src/main/java/com/ahmeddebbech/aries_messenger/LoginActivity.java
@@ -17,6 +17,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
@@ -66,9 +68,11 @@ public class LoginActivity extends AppCompatActivity {
             IdpResponse response = IdpResponse.fromResultIntent(data);
             if (resultCode == RESULT_OK) {
                 loggedInUser = new LoggedInUser(FirebaseAuth.getInstance().getCurrentUser());
-
                 Toast toast = Toast.makeText(this, "Welcome " + loggedInUser.getFirebaseUserObject().getDisplayName(), Toast.LENGTH_SHORT);
                 toast.show();
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference username = database.getReference("/users/$uid");
+                username.setValue(loggedInUser.getFirebaseUserObject().getDisplayName());
                 Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
             } else {
---
+++ app/src/main/java/com/ahmeddebbech/aries_messenger/database/Database.java
@@ -0,0 +1,4 @@
+package com.ahmeddebbech.aries_messenger.database;
+
+public class Database {
+}
