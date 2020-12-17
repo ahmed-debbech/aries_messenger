6294d6c064ebbdf0f1b104a35b5038c079de0144
ahmed-debbech <debbech.ahmed@gmail.com>
2020-12-17 23:31:04 +0100

    customize some featuresg

---
+++ app/src/main/java/com/ahmeddebbech/aries_messenger/activities/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.ahmeddebbech.aries_messenger.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ahmeddebbech.aries_messenger.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
