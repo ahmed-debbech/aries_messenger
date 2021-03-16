b21bf99c2cbb3bdeb6f917d8242cd2991b472997
ahmed-debbech <debbech.ahmed@gmail.com>
2021-03-16 21:09:51 +0100

    add toolbar for conversation UI

--- app/src/main/java/com/ahmeddebbech/aries_messenger/views/activities/ConversationActivity.java
+++ app/src/main/java/com/ahmeddebbech/aries_messenger/views/activities/ConversationActivity.java
@@ -1,12 +1,19 @@
 package com.ahmeddebbech.aries_messenger.views.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 import com.ahmeddebbech.aries_messenger.R;
 
 public class ConversationActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
