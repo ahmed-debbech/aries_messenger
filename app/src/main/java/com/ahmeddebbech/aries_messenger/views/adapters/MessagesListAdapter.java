901937541f9e7362a36919e3d80f28b8cd74eec6
ahmed-debbech <debbech.ahmed@gmail.com>
2021-05-06 00:48:11 +0100

    add edit panel in messages

--- app/src/main/java/com/ahmeddebbech/aries_messenger/views/adapters/MessagesListAdapter.java
+++ app/src/main/java/com/ahmeddebbech/aries_messenger/views/adapters/MessagesListAdapter.java
@@ -3,11 +3,15 @@ package com.ahmeddebbech.aries_messenger.views.adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ahmeddebbech.aries_messenger.R;
@@ -41,6 +45,11 @@ public class MessagesListAdapter extends RecyclerView.Adapter<MessagesListAdapte
         private ImageView msg_image;
         private ImageView msg_status;
         private Message ref;
+        private RelativeLayout msg_edit_panel;
+        private EditText msg_edit;
+        private TextView msg_edit_button;
+        private TextView msg_cancel_button;
+        private ConstraintLayout msg_main_panel;
 
         public MessageViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -50,6 +59,12 @@ public class MessagesListAdapter extends RecyclerView.Adapter<MessagesListAdapte
             background = itemView.findViewById(R.id.msg_card);
             msg_image = itemView.findViewById(R.id.msg_img);
             msg_status = itemView.findViewById(R.id.msg_status);
+            msg_edit_panel = itemView.findViewById(R.id.msg_edit_panel);
+            msg_edit = itemView.findViewById(R.id.msg_edit);
+            msg_edit_button = itemView.findViewById(R.id.msg_edit_button);
+            msg_main_panel = itemView.findViewById(R.id.msg_main_panel);
+            msg_edit_button = itemView.findViewById(R.id.msg_edit_button);
+            msg_cancel_button = itemView.findViewById(R.id.msg_cancel_button);
         }
 
     }
@@ -78,7 +93,9 @@ public class MessagesListAdapter extends RecyclerView.Adapter<MessagesListAdapte
             holder.background.setOnLongClickListener(new View.OnLongClickListener() {
                 @Override
                 public boolean onLongClick(View v) {
-                    hld.content.setText("Pressed!");
+                    hld.msg_edit.setText(hld.content.getText());
+                    hld.msg_main_panel.setVisibility(View.INVISIBLE);
+                    hld.msg_edit_panel.setVisibility(View.VISIBLE);
                     return true;
                 }
             });
